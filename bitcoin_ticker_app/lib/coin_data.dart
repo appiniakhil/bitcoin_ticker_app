596238eb3b4c7bbb5c90899d996a2e315f7a31d5
appiniakhil <appiniakhil01@gmail.com>
2023-03-31 12:21:43 +0530

    .

--- bitcoin_ticker_app/lib/coin_data.dart
+++ bitcoin_ticker_app/lib/coin_data.dart
@@ -1,6 +1,3 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -31,28 +28,10 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-const apiKey = "71CC0E49-2879-4B0F-B982-650ABDA9EBAA";
-const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
 
-class CoinData {
-  Future getCoinData(String selectedCurrency) async {
-    Map<String, String> cryptoPrices = {};
-    for (String crypto in cryptoList) {
-      String requestURL =
-          '$coinAPIURL/$crypto/$selectedCurrency?apikey=$apiKey';
-      http.Response response = await http.get(Uri.parse(requestURL));
-      if (response.statusCode == 200) {
-        var decodedData = jsonDecode(response.body);
-        double price = decodedData['rate'];
-        cryptoPrices[crypto] = price.toStringAsFixed(0);
-      } else {
-        print(response.statusCode);
-        throw 'Problem with the get request';
-      }
-    }
-    return cryptoPrices;
-  }
-}
+
+
+
 
 
 
